3bd3b731a3d4d332ac86d207d6d7da1862a6b92d
Jose Colque <jogacolhue@tutanota.com>
2020-06-21 22:18:12 -0500

    first commit

---
+++ InventoryService/Readme.md
@@ -0,0 +1,22 @@
+﻿https://www.syncfusion.com/blogs/post/how-to-build-crud-rest-apis-with-asp-net-core-3-1-and-entity-framework-core-create-jwt-tokens-and-secure-apis.aspx
+
+Create Table Products(
+ProductId Int Identity(1,1) Primary Key,
+Name Varchar(100) Not Null,
+Category Varchar(100),
+Color Varchar(20),
+UnitPrice Decimal Not Null,
+AvailableQuantity Int Not Null)
+GO
+Create Table UserInfo(
+UserId Int Identity(1,1) Not null Primary Key,
+FirstName Varchar(30) Not null,
+LastName Varchar(30) Not null,
+UserName Varchar(30) Not null,
+Email Varchar(50) Not null,
+Password Varchar(20) Not null,
+RefreshToken Varchar(max) null,
+CreatedDate DateTime Default(GetDate()) Not Null)
+GO
+Insert Into UserInfo(FirstName, LastName, UserName, Email, Password) 
+Values ('Inventory', 'Admin', 'InventoryAdmin', 'InventoryAdmin@abc.com', '$admin@2017')
---
+++ InventoryService/Startup.cs
@@ -0,0 +1,71 @@
+using InventoryService.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+
+namespace InventoryService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("InventoryDatabase");
+            services.AddDbContextPool<InventoryContext>(options => options.UseSqlServer(connection));
+
+            services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
